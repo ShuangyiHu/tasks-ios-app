4143cd2a5cb2a00f43ecf8d37a89d9f2c92090e7
ShuangyiHu <hu.shua@northeastern.edu>
2024-09-15 10:41:14 -0700

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by 胡爽怡 on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
